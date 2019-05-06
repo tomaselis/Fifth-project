94433bb516a2a8f61eaa8a1e39c329ea7f556bb2
tomaselis <tsabaliauskis@hotmail.co.uk>
2019-05-06 19:38:56 +0300

    antras komitas

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
 
 <?php
 
-$img = 'photos/explosion.png';
+$siukslines_turis = 40; //dm3
+$siuksliu_turis_avg_per_day = 15;
+$max_kaupo_turis = rand(10, 20);
+
+
+$dienu = ($siukslines_turis + $max_kaupo_turis) / $siuksliu_turis_avg_per_day;
+$tikslus_sk_dienu = round($dienu);
+
+/*for ($siukslines_turys; $siuksliu_turis >= 60; $siukslines_turis - $max_kaupo_turis)*/
+$date = date('Y-m-d', strtotime('+' . $tikslus_sk_dienu . 'days'));
 
 ?>
 
@@ -14,14 +23,9 @@ $img = 'photos/explosion.png';
     <meta charset="UTF-8">
     <title>Fifth project</title>
     <style>
-
-
     </style>
 </head>
 <body>
-<img src="<?php print $img; ?>">
-<img src="<?php print $img; ?>">
-<img src="<?php print $img; ?>">
-<img src="<?php print $img; ?>">
+<p><?php print 'Po ' . $date   . ' pirk geliu ir sampano jeigu nori kad tavo zmona pati geles issinestu'?></p>
 </body>
-</html>
+</html
