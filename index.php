945b9c1534b95979d981eb55a119f9f2b0173d31
tomaselis <tsabaliauskis@hotmail.co.uk>
2019-05-06 19:02:30 +0300

    pirmas komitas

---
+++ index.php
@@ -0,0 +1,27 @@
+
+
+<?php
+
+$img = 'photos/explosion.png';
+
+?>
+
+
+
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Fifth project</title>
+    <style>
+
+
+    </style>
+</head>
+<body>
+<img src="<?php print $img; ?>">
+<img src="<?php print $img; ?>">
+<img src="<?php print $img; ?>">
+<img src="<?php print $img; ?>">
+</body>
+</html>
